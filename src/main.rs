852f2f61b9f0f66634bfd5a4c60d5c35e89f81cd
eric wolf <coyocanid@gmail.com>
2025-03-16 15:59:36 -0700

    updated macros

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,19 @@ use std::collections::HashMap;
 
 init_objects!();
 
+struct Config {
+    min_word_length: usize,
+    max_word_length: usize,
+
+    min_coinc_cue_perc: usize,
+    max_coinc_cue_perc: usize,
+
+    min_coinc_cue_freq: usize,
+
+    min_overall_cue_perc: f32,
+    max_overall_cue_perc: f32,
+}
+
 #[derive(Serialize, Deserialize, Getters, Debug)]
 struct CueStore {
     cues: Vec<String>,
@@ -44,7 +57,7 @@ struct Exemplars {
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use regex::Regex;
-fn load_exems(file_name: &str, object_store: &ObjectStore)
+fn load_exems(file_name: &str, object_store: &ObjectStore, config: &Config)
               -> Result<Box<Obj<Exemplars>>,RecordStoreError>
 {
     let binding = object_store.record_store_binding();
@@ -52,7 +65,7 @@ fn load_exems(file_name: &str, object_store: &ObjectStore)
 
     let mut root = object_store.fetch_root_rs(&mut record_store);
 
-    if let Some(ObjTypeOption::Ref(exems_ref)) = root.get("exemplars") {
+    if let Some(ObjTypeOption::Ref(exems_ref)) = root.get("exems") {
         let exems = object_store.fetch_rs(&mut record_store, exems_ref.id)?;
         let glo_uniq_exems = object_store.fetch_rs::<RefVec>(&mut record_store, exems.get_glo_uniq_exems_ref().id )?;
         eprintln!( "ALREADY HAVE THE exems {}. id {}", glo_uniq_exems.data.vec.len(), exems.id );
@@ -101,18 +114,22 @@ fn load_exems(file_name: &str, object_store: &ObjectStore)
             let mut seq_cue_idxs: Vec<u32> = Vec::new();
 
             for word in &words {
-                let word_str = word.to_string();
-                match cue2idx.get(&word_str) {
-                    None => {
-                        let idx: u32 = cues.len()  as u32;
-                        cues.push(word_str.clone());
-                        cue2idx.insert(word_str.clone(), idx);
-                        seq_cue_idxs.push(idx);
-                        glo_tot_freq[idx as usize] = 1;
-                    },
-                    Some(idx) => {
-                        seq_cue_idxs.push(idx.clone());
-                        glo_tot_freq[*idx as usize] += 1;
+                if word.len() >= config.min_word_length
+                    && word.len() <= config.max_word_length 
+                { 
+                    let word_str = word.to_string();
+                    match cue2idx.get(&word_str) {
+                        None => {
+                            let idx: u32 = cues.len()  as u32;
+                            cues.push(word_str.clone());
+                            cue2idx.insert(word_str.clone(), idx);
+                            seq_cue_idxs.push(idx);
+                            glo_tot_freq[idx as usize] = 1;
+                        },
+                        Some(idx) => {
+                            seq_cue_idxs.push(idx.clone());
+                            glo_tot_freq[*idx as usize] += 1;
+                        }
                     }
                 }
             }
@@ -201,31 +218,89 @@ fn load_exems(file_name: &str, object_store: &ObjectStore)
     Ok(exems)
 }
 
-fn chunk_cues_by_freq( freqs: &Vec<u32>, min_cue_idx: u32, max_cue_idx: u32 ) -> Vec<u32> {
+fn chunk_cues_by_freq( freqs: &Vec<u32>, min_cue_idx: usize, max_cue_idx: usize ) -> Vec<usize> {
+    let max_chunk_size = 5000;
+    let thresh_fract = 700;
+
     let mut tot = 0;
     for idx in (min_cue_idx..max_cue_idx).collect::<Vec<_>>() {
-        
+        tot += freqs[idx];
     }
-    Vec::new()
+
+    let thresh = tot / thresh_fract;
+    let mut chunk_number_freqs = 0;
+
+    let mut cue_chunks: Vec<usize> = Vec::new();
+    let mut f = 0;
+    for idx in (min_cue_idx..max_cue_idx).collect::<Vec<_>>() {
+        f += freqs[idx];
+        chunk_number_freqs += 1;
+        if f > thresh || chunk_number_freqs > max_chunk_size {
+            cue_chunks.push( chunk_number_freqs );
+            chunk_number_freqs = 0;
+            f = 0;
+        }
+    }
+
+    cue_chunks
 }
 
-/*
-fn coincs(exem_os: &ObjectStore, coinc_os: &ObjectStore) -> Result<(),RecordStoreError>
+fn find_coincs(exem_os: &ObjectStore, coinc_os: &ObjectStore, config: &Config) 
+               -> Result<Box<Obj<U32Vec>>,RecordStoreError>
 {
-    
+    let binding = exem_os.record_store_binding();
+    let mut exem_rs = binding.lock().unwrap();
+    let mut exem_root = exem_os.fetch_root_rs(&mut exem_rs);
+
+    let binding = coinc_os.record_store_binding();
+    let mut coinc_rs = binding.lock().unwrap();
+    let mut coinc_root = coinc_os.fetch_root_rs(&mut coinc_rs);
+
+    if let Some(ObjTypeOption::Ref(coincs_ref)) = coinc_root.get("coincs") {
+        let coincs = coinc_os.fetch_rs::<U32Vec>(&mut coinc_rs, coincs_ref.id )?;
+        eprintln!("Found {} coincs", coincs.data.vec.len() );
+        return Ok(coincs);
+    }
+
+
+    if let Some(ObjTypeOption::Ref(exems_ref)) = exem_root.get("exems") {
+        let exems = exem_os.fetch_rs(&mut exem_rs, exems_ref.id)?;
+        let loc_tot_freq = exem_os.fetch_rs::<U32Vec>(&mut exem_rs, exems.get_loc_tot_freq_ref().id )?;
+
+//        let chunks = chunk_cues_by_freq( loc_tot_freq, config );
+    } 
+      
+    Err(RecordStoreError::ObjectStore("HBEEEL".to_string()))
 }
-*/
+
 fn main() {
+
+    let conf = Config {
+        min_word_length: 2,
+        max_word_length: 12,
+
+        min_coinc_cue_perc: 1,
+        max_coinc_cue_perc: 30,
+
+        min_coinc_cue_freq: 100,
+
+        min_overall_cue_perc: 0.5,
+        max_overall_cue_perc: 40.0,
+
+    };
+
+
+// no word limits: 943M	data, 
 
     let exem_os = ObjectStore::new("./data/exems");
     let coinc_os = ObjectStore::new("./data/coinc");
 
-    let exems = load_exems("./source_data/all_articles.txt", &exem_os).expect("got exems");
+    let exems = load_exems("./source_data/all_articles.txt", &exem_os, &conf).expect("got exems");
 
     let seq_exems = exem_os.fetch::<RefVec>( exems.get_glo_seq_exems_ref().id ).expect("No way");
     eprintln!(" exems howmany ? {}", seq_exems.data.vec.len() );
 
-    
+    find_coincs( &exem_os, &coinc_os, &conf ).expect("could not coincs");
     
     println!("HI");
 }
