2d32622031f41e5c2c3efed7f9a2eae11c733c95
eric wolf <coyocanid@gmail.com>
2025-03-16 15:59:36 -0700

    updated macros

--- src/object_store.rs
+++ src/object_store.rs
@@ -276,14 +276,14 @@ struct Canary {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum ObjectTypeOption {
-    Bool,
-    I32,
-    I64,
-    U32,
-    U64,
-    F32,
-    F64,
-    String,
+    Bool(bool),
+    I32(i32),
+    I64(i64),
+    U32(u32),
+    U64(u64),
+    F32(f32),
+    F64(f64),
+    String(String),
 }
 
 #[derive(Serialize, Deserialize, Debug, Getters)]
