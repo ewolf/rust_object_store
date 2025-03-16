d3aaa1b0c3eb592898be0db62840dd55f596a977
eric wolf <coyocanid@gmail.com>
2025-03-16 15:59:36 -0700

    updated macros

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,16 @@ struct Exemplars {
     cuestore_ref: Ref,
     seq_exems_ref: Ref,
     uniq_exems_ref: Ref,
+    global_freqs_ref: Ref,
+    per_exem_freqs_ref: Ref,
 }
 
 
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use regex::Regex;
-fn load_exems(file_name: &str, object_store: &ObjectStore) 
-              -> Result<Box<Obj<Exemplars>>,RecordStoreError> 
+fn load_exems(file_name: &str, object_store: &ObjectStore)
+              -> Result<Box<Obj<Exemplars>>,RecordStoreError>
 {
     let binding = object_store.record_store_binding();
     let mut record_store = binding.lock().unwrap();
@@ -52,6 +54,9 @@ fn load_exems(file_name: &str, object_store: &ObjectStore)
 
     let mut cues: Vec<String> = Vec::new();
     let mut cue2idx: HashMap<String,u32> = HashMap::new();
+
+    let mut global_freqs: Vec<u32> = Vec::new();
+    let mut per_exem_freqs: Vec<u32> = Vec::new();
 
     let mut seq_exems_vec: Vec<Ref> = Vec::new();
     let mut uniq_exems_vec: Vec<Ref> = Vec::new();
@@ -87,9 +92,13 @@ fn load_exems(file_name: &str, object_store: &ObjectStore)
                         cues.push(word_str.clone());
                         cue2idx.insert(word_str.clone(), idx);
                         seq_cue_idxs.push(idx);
+                        global_freqs.insert(idx as usize,1);
                     },
                     Some(idx) => {
                         seq_cue_idxs.push(idx.clone());
+                        if let Some(old_count) = global_freqs.get(*idx as usize) {
+                            global_freqs.insert(*idx as usize,1 + old_count);
+                        }
                     }
                 }
             }
@@ -98,33 +107,49 @@ fn load_exems(file_name: &str, object_store: &ObjectStore)
             uniq_cue_idxs.sort_unstable();
             uniq_cue_idxs.dedup();
 
+            for idx in &uniq_cue_idxs {
+                match per_exem_freqs.get(*idx as usize) {
+                    Some(old_count) => {
+                        per_exem_freqs.insert(*idx as usize,1 + old_count);
+                    },
+                    None => {
+                        per_exem_freqs.insert(*idx as usize,1);
+                    }
+                }
+            }
+
             let seq_exem = object_store
                 .new_obj_rs( &mut record_store, U32Vec { vec: seq_cue_idxs } )?;
-            
+
             seq_exems_vec.push( seq_exem.make_ref() );
 
 
             let uniq_exem = object_store
                 .new_obj_rs( &mut record_store, U32Vec { vec: uniq_cue_idxs } )?;
-            
+
             uniq_exems_vec.push( uniq_exem.make_ref() );
 
             needs_title = true;
         }
     }
 
-    let cuestore = object_store.new_obj_rs( &mut record_store, 
+    let cuestore = object_store.new_obj_rs( &mut record_store,
                                              CueStore { cues, cue2idx } )?;
-    let seq_exems = object_store.new_obj_rs( &mut record_store, 
+    let seq_exems = object_store.new_obj_rs( &mut record_store,
                                               RefVec { vec: seq_exems_vec } )?;
-    let uniq_exems = object_store.new_obj_rs( &mut record_store, 
+    let uniq_exems = object_store.new_obj_rs( &mut record_store,
                                                RefVec { vec: uniq_exems_vec } )?;
-
-    let exems = object_store.new_obj_rs( &mut record_store, 
-                                          Exemplars { 
+    let global_freqs = object_store.new_obj_rs( &mut record_store,
+                                                 U32Vec { vec: global_freqs } )?;
+    let per_exem_freqs = object_store.new_obj_rs( &mut record_store,
+                                                   U32Vec { vec: per_exem_freqs } )?;
+    let exems = object_store.new_obj_rs( &mut record_store,
+                                          Exemplars {
                                               cuestore_ref: cuestore.make_ref(),
                                               seq_exems_ref: seq_exems.make_ref(),
                                               uniq_exems_ref: uniq_exems.make_ref(),
+                                              global_freqs_ref: global_freqs.make_ref(),
+                                              per_exem_freqs_ref: per_exem_freqs.make_ref(),
                                           } )?;
 
     root.put( "exems", exems.make_ref_opt() );
@@ -136,12 +161,13 @@ fn load_exems(file_name: &str, object_store: &ObjectStore)
 fn main() {
 
     let exem_os = ObjectStore::new("./data/exems");
-    
+
     let exems = load_exems("./source_data/all_articles.txt", &exem_os).expect("got exems");
 
     let seq_exems = exem_os.fetch::<RefVec>( exems.get_seq_exems_ref().id ).expect("No way");
     eprintln!(" exems howmany ? {}", seq_exems.data.vec.len() );
 
-
+    
+    
     println!("HI");
 }
